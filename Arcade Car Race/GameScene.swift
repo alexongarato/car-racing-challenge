114da06d3ef3112466b4996a0310a238ae44141c
Alex Ongarato <alex.ongarato@w3haus.com.br>
2015-03-08 23:10:01 -0300

    Início de tudo

---
+++ Arcade Car Race/GameScene.swift
@@ -0,0 +1,51 @@
+//
+//  GameScene.swift
+//  Arcade Car Race
+//
+//  Created by Alex Ongarato on 08/03/15.
+//  Copyright (c) 2015 Alex Ongarato. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+    
+    /*
+    func showPrepareMessage()
+    {
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Ready";
+        myLabel.fontSize = 50;
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        self.addChild(myLabel)
+    }
+    */
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
